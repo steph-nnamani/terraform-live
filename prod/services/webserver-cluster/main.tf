f601a64bc750011e693f1c7d97ff6f550c3ad5ca
steph-nnamani@gmail.com <steph-nnamani@gmail.com>
2025-05-11 21:12:49 -0500

    Updated the main.tf

--- prod/services/webserver-cluster/main.tf
+++ prod/services/webserver-cluster/main.tf
@@ -13,7 +13,7 @@ provider "aws" {
 
 module "webserver_cluster" {
   #source = "C:/Users/xtrah/terraform-up-and-running-by-Yev-Brikman/chpt5-loops-conditionals-deployment/modules/services/webserver-cluster"
-  source = "github.com/steph-nnamani/modules///services/webserver-cluster?ref=v3.1.1"  # Confirm the version
+  source = "github.com/steph-nnamani/modules//services/webserver-cluster?ref=v3.1.1"  # Confirm the version
   #    source = "git::ssh://git@github.com/steph-nnamani/modules.git//services/webserver-cluster?ref=v1.1.0"   # Confirm the version
   ami         = "ami-0866a3c8686eaeeba"
   server_text = "Happy Mothers' Sunday!!!"
