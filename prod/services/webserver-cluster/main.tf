41eafca9acb0171b32a7a7ace8aa001984624fc0
steph-nnamani@gmail.com <steph-nnamani@gmail.com>
2025-05-12 21:48:32 -0500

    Changed ther module version to v4.0.1

--- prod/services/webserver-cluster/main.tf
+++ prod/services/webserver-cluster/main.tf
@@ -13,7 +13,7 @@ provider "aws" {
 
 module "webserver_cluster" {
   #source = "C:/Users/xtrah/terraform-up-and-running-by-Yev-Brikman/chpt5-loops-conditionals-deployment/modules/services/webserver-cluster"
-  source = "github.com/steph-nnamani/modules///services/webserver-cluster?ref=v4.0.0" 
+  source = "github.com/steph-nnamani/modules///services/webserver-cluster?ref=v4.0.1" 
   # source = "git::ssh://git@github.com/steph-nnamani/modules.git//services/webserver-cluster?ref=v3.1.1"   # Confirm the version
   ami         = "ami-0866a3c8686eaeeba"
   server_text = "Happy Mothers' Sunday!!!"
