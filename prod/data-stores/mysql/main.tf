c18451627cc06ab7bcc9a5206139417021611c6d
steph-nnamani@gmail.com <steph-nnamani@gmail.com>
2025-05-19 16:05:45 -0500

    Updated mysql code to use data-store module

--- prod/data-stores/mysql/main.tf
+++ prod/data-stores/mysql/main.tf
@@ -1,6 +1,18 @@
+# provider "aws" {
+#     region = "us-east-1"
+# #    profile = "terraform"
+# }
+
 provider "aws" {
     region = "us-east-1"
-#    profile = "terraform"
+    profile = "terraform"
+    alias = "primary"
+}
+
+provider "aws" {
+    region = "us-east-2"
+    profile = "terraform"
+    alias = "replica"
 }
 
 # You can use aws_secretsmanager_secret_version data source to 
@@ -16,20 +28,54 @@ locals {
     db_creds = jsondecode(data.aws_secretsmanager_secret_version.creds.secret_string)
 }
 
-# You can now use the local variable db_creds to access the username and passwor
-resource "aws_db_instance" "example" {
+module "mysql_primary" {
+    # source = "../../../../../staging/modules/data-stores/mysql"
+    #source = "C:/Users/xtrah/staging/modules/data-stores/mysql"
+    source = "github.com/steph-nnamani/modules//data-stores/mysql?ref=v1.1.0-data-stores" 
+    # AWS Provider Configuration for this module
+    providers = {
+        aws = aws.primary
+    }
     identifier_prefix = "${var.rds}-instance"
-    engine = "mysql"
-    engine_version = "8.0"
-    allocated_storage = 10
-    instance_class = var.instance_class
-    skip_final_snapshot = true
     db_name = "${var.rds}_database"
+    # db_name = "prod_db"
+    # db_username = var.db_username
+    # db_password = var.db_password
+    db_username = local.db_creds.db_username 
+    db_password = local.db_creds.db_password
 
-    # disable backups to create DB faster
-    backup_retention_period = 0
-
-    # db attribute names
-    username = local.db_creds.username 
-    password = local.db_creds.password
+    # Must be enabled to support replication
+    backup_retention_period = 1
 }
+
+module "mysql_replica" {
+    # source = "../../../../../staging/modules/data-stores/mysql"
+    source = "C:/Users/xtrah/staging/modules/data-stores/mysql"
+
+    # AWS Provider Configuration for this module
+    providers = {
+        aws = aws.replica
+    }
+
+    # Make this a replica of the primary
+    replicate_source_db = module.mysql_primary.arn
+}
+
+
+# # You can now use the local variable db_creds to access the username and passwor
+# resource "aws_db_instance" "example" {
+#     identifier_prefix = "${var.rds}-instance"
+#     engine = "mysql"
+#     engine_version = "8.0"
+#     allocated_storage = 10
+#     instance_class = var.instance_class
+#     skip_final_snapshot = true
+#     db_name = "${var.rds}_database"
+
+#     # disable backups to create DB faster
+#     backup_retention_period = 0
+
+#     # db attribute names
+#     username = local.db_creds.username 
+#     password = local.db_creds.password
+# }
--- prod/data-stores/mysql/outputs.tf
+++ prod/data-stores/mysql/outputs.tf
@@ -1,9 +1,38 @@
-output "address" {
-  value = aws_db_instance.example.address
-  description = "Connect to the database at this endpoint"
+# output "address" {
+#   value = aws_db_instance.example.address
+#   description = "Connect to the database at this endpoint"
+# }
+
+# output "port" {
+#   value = aws_db_instance.example.port
+#   description = "The port the database is listening on"
+# }
+
+output "primary_address" {
+  value       = module.mysql_primary.address
+  description = "Connect to the primary database at this endpoint"
+}
+output "replica_address" {
+  value       = module.mysql_replica.address
+  description = "Connect to the replica database at this endpoint"
 }
 
-output "port" {
-  value = aws_db_instance.example.port
-  description = "The port the database is listening on"
+output "primary_port" {
+  value       = module.mysql_primary.port
+  description = "The primary database port"
+}
+
+output "replica_port" {
+  value       = module.mysql_replica.port
+  description = "The replica database port"
+}
+
+output "primary_arn" {
+    value       = module.mysql_primary.arn
+    description = "The primary database arn"
+}
+
+output "replica_arn" {
+    value       = module.mysql_replica.arn
+    description = "The replica database arn"
 }
